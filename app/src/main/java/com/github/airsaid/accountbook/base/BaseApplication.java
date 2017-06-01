61d71d312e17ced37603ff4cb61d725db2efc4e3
airsaid <airsaid1024@gmail.com>
2017-06-01 22:50:01 +0800

    update readme

--- app/src/main/java/com/github/airsaid/accountbook/base/BaseApplication.java
+++ app/src/main/java/com/github/airsaid/accountbook/base/BaseApplication.java
@@ -23,6 +23,15 @@ import org.greenrobot.greendao.database.Database;
  * @github https://github.com/airsaid
  * @date 2017/4/13
  * @desc Application 基础类
+ *
+ * 【注意事项：】
+ *
+ * 由于应用当中用到了 leancloud 和 bugly 服务，其 appid、appkey 由于私密原因已经隐藏。
+ * 请在 local.properties 配置如下信息，配置后即可正常运行项目：
+ *
+ * leancloud.appid=你在 leancloud 平台申请的 appid
+ * leancloud.appkey=你在 leancloud 平台申请的 appkey
+ * bugly.appid=你在 bugly 平台申请的 appkey
  */
 public class BaseApplication extends Application{
 
@@ -77,11 +86,7 @@ public class BaseApplication extends Application{
     }
 
     /**
-     * 初始化 leancloud 服务。
-     * APP_ID、APP_KEY 由于私密原因已经隐藏，请在 local.properties 配置如下信息，配置后即可正常运行。
-     *
-     * leancloud.appid=你在 leancloud 平台申请的 appid
-     * leancloud.appkey=你在 leancloud 平台申请的 appkey
+     * 初始化 leancloud
      */
     private void initLeancloud() {
         try {
